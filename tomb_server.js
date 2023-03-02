8efbcb0947d13cd5dfa588381907ab8396d2b57f
kf2001 <ennio286@gmail.com>
2023-03-02 17:41:33 +0100

    vue

--- tomb_server.js
+++ tomb_server.js
@@ -24,15 +24,20 @@ let rimasti = [...palline]
 
 app.use(express.static('./public'));
 
+app.get("/reboot", (req, res) => {
+
+    process.exit(1)
+})
 var numero = 0
 
 var activeClients = 0;
 var maxClients = 10;
 
-var args = [];
-
-maxClients = 10;
+maxClients = 100;
 var status = -2
+
+console.log("started")
+
 
 io.sockets.on('connection', function (socket) {
 
@@ -58,15 +63,15 @@ io.sockets.on('connection', function (socket) {
 
         socket.on('chiama', function (msg) {
 
-          
+
             io.sockets.emit("combinaz", { comb: msg, nick: socket.nickname });
 
 
-socket.vincite.push(msg.charAt(0))
+            socket.vincite.push(msg.charAt(0))
 
             //Verifica combinazione
 
-           
+
         });
 
 
@@ -92,7 +97,7 @@ socket.vincite.push(msg.charAt(0))
         socket.on('estrai', function () {
 
             let pallina = casuale(rimasti.length)
-            
+
             let estratta = rimasti.splice(pallina, 1)[0]
 
             estratti.push(pallina)
@@ -104,21 +109,35 @@ socket.vincite.push(msg.charAt(0))
 
         socket.on('tabella', function () {
 
-            let strh="<table class='tbl'>"
-         
-            allClients.forEach(function (s) {
-              strh+="<tr>"
-              strh+="<td>"+s.nickname+"</td>"
-              strh+="<td>"+Math.floor(s.cartelle.length/27)+"</td>"
-              strh+="<td>"+s.vincite+"</td>"
+            let strh = "<table class='tbl'>"
+            strh += "<tr><th>nome</th> <th>cartelle</th><th>id</th> <th>vincite</th><th>ip</th>  "
+            allClients.forEach(function (s,idx) {
+                strh += "<tr>"
+                strh += "<td>" + s.nickname + "</td>"
+                strh += "<td>" + Math.floor(s.cartelle.length / 27) + "</td>"
+                strh += "<td>" + s.id + "</td>"
+                strh += "<td>" + s.vincite + "</td>"
+                strh += "<td>" + s.ip + "</td>"
+
+                strh += "<td><button onclick='disconn("+idx+")'>Disconnetti</button></td>"
             });
-            strh+="</table>"
+            strh += "</table>"
 
             socket.emit('tabhtml', strh)
 
         });
 
-    
+        socket.on('disconnetti', function (msg) {
+
+            console.log(msg)
+
+         allClients[msg].disconnect()
+
+
+        });
+
+
+
         socket.on('regolamento', function (msg) {
 
             allClients.forEach(function (s) {
@@ -137,7 +156,8 @@ socket.vincite.push(msg.charAt(0))
             socket.nickname = msg;
             socket.cartelle = []
             socket.colori = []
-            socket.vincite=[]
+            socket.vincite = []
+            socket.ip=socket.conn.remoteAddress
             if (amministratore == 0) { amministratore = 1; socket.amministratore = socket.nickname; socket.emit("amministratore", 1) }
 
 
@@ -175,7 +195,8 @@ function comprataCartella(numCartella, numGioc, sck) {
 
 function casuale(n) { return Math.floor(n * Math.random()) }
 
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 8040;
 
 
 httpServer.listen(port);
